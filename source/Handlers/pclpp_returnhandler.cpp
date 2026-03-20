a1e86bbffb6c64fa6cabef61c772e292e389a60d
Dimo <dimobogula13053@gmail.com>
2026-03-20 18:40:05 +0100

    add return

--- source/Handlers/pclpp_returnhandler.cpp
+++ source/Handlers/pclpp_returnhandler.cpp
@@ -9,4 +9,12 @@ void PCLPP_ReturnHandler::OnToken(PCLPP* PCLPP, const std::string& token)
         PCLPP->assembly.MOVRImm(0, stoi(other));
         std::string final = PCLPP->tokenizer.tokens.Advance();
     }
+    else if (token == "return")
+    {
+        std::string other = PCLPP->tokenizer.tokens.Advance();
+        PCLPP->assembly.MOVRImm(0, stoi(other));
+        PCLPP->assembly.POP_LR();
+        PCLPP->assembly.BXLR();
+        std::string final = PCLPP->tokenizer.tokens.Advance();
+    }
 }
