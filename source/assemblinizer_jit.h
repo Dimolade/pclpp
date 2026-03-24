6964beaf240129d267bbb28c9039d669cb3f96d6
Dimo <dimobogula13053@gmail.com>
2026-03-24 22:00:52 +0100

    oopsie woopsie, LATCHKUMMM

--- source/assemblinizer_jit.h
+++ source/assemblinizer_jit.h
@@ -7,7 +7,7 @@ typedef int(*Fn_t)(uint32_t);
 
 struct Assemblinizer
 {
-    inline static Fn_t Get(Assembly ASM, uint32_t index = 0)
+    inline static Fn_t Get(Assembly& ASM, uint32_t index = 0)
     {
         if (ASM.startAddress == 0)
         {
@@ -18,7 +18,7 @@ struct Assemblinizer
         }
         return (Fn_t)ASM.startAddress;
     }
-    inline static int Run(Assembly ASM, uint32_t index = 0, uint32_t input = 0)
+    inline static int Run(Assembly& ASM, uint32_t index = 0, uint32_t input = 0)
     {
         if (ASM.startAddress == 0)
         {
