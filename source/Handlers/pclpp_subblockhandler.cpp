0b4defb211f5fa6505ba9cfe1afc78f0f35d6e0c
Dimo <dimobogula13053@gmail.com>
2026-04-02 12:49:20 +0200

    Update pclpp_subblockhandler.cpp

--- source/Handlers/pclpp_subblockhandler.cpp
+++ source/Handlers/pclpp_subblockhandler.cpp
@@ -2,8 +2,6 @@
 #include "../assemblinizer_jit.h"
 #include "../plcpp_compiler.hpp"
 
-#define mainhandler_debug
-
 void PCLPP_MainHandler::OnToken(PCLPP* PCLPP, const std::string& token)
 {
     if (token == "end" && PCLPP->inBlock)
